1703a019fac9e6897b70de3fd57e316650042c8d
Pritam <pritam.sarkar@medlife.com>
2020-09-04 02:56:03 +0530

    Solve 3Sept leetcode monthly

---
+++ LeetCode/MonthlyChallenge/Sep2020/repeatedSubstringPattern.js
@@ -0,0 +1,60 @@
+/***
+ Repeated Substring Pattern
+
+ Given a non-empty string check if it can be constructed by taking a substring of it
+ and appending multiple copies of the substring together.
+ You may assume the given string consists of lowercase English letters only and its length will not exceed 10000.
+
+
+
+ Example 1:
+
+ Input: "abab"
+ Output: True
+ Explanation: It's the substring "ab" twice.
+
+ Example 2:
+
+ Input: "aba"
+ Output: False
+
+ Example 3:
+
+ Input: "abcabcabcabc"
+ Output: True
+ Explanation: It's the substring "abc" four times. (And the substring "abcabc" twice.)
+
+ */
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var repeatedSubstringPattern = function(s) {
+    if(s.length < 2) {
+        return false;
+    }
+    let rep_len = Math.ceil(s.length/2);
+    while(rep_len > 0) {
+        let is_rep = repetition(s.substring(0,rep_len), s.substring(rep_len, s.length));
+        if(is_rep) {
+            return true;
+        } else {
+            rep_len--;
+        }
+    }
+    return false;
+};
+
+function repetition(rep, rest) {
+    if(rep === rest) {
+        return true;
+    }
+    if(rest.startsWith(rep)) {
+        return repetition(rep, rest.substring(rep.length));
+    } else {
+        return false;
+    }
+}
+
+module.exports = repeatedSubstringPattern;
---
+++ LeetCode/MonthlyChallenge/Sep2020/repeatedSubstringPattern.spec.js
@@ -0,0 +1,15 @@
+const {test} = require("@jest/globals");
+
+const repeatedSubstringPattern = require("./repeatedSubstringPattern");
+
+test("repeatedSubstringPattern 1", () => {
+    expect(repeatedSubstringPattern("abab")).toBe(true);
+});
+
+test("repeatedSubstringPattern 2", () => {
+    expect(repeatedSubstringPattern("aba")).toBe(false);
+});
+
+test("repeatedSubstringPattern 3", () => {
+    expect(repeatedSubstringPattern("abcabcabcabc")).toBe(true);
+});
